e878ab13a72c7bcca6383b3af0557005e981c0e9
fikrizal <fikrizalwahyudi@gmail.com>
2019-09-03 11:40:42 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import App from './App.svelte';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../public/pdf.css';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
+});
+
+
+	 
+
+
+window.app = app;
+
+export default app;
