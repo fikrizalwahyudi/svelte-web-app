0e29bb342054d813a0d5234a53fb6ae4070f2006
luckyramdani007 <luckyramdani007@gmail.com>
2019-09-06 13:00:56 +0700

    fixing minor

--- prudential1/src/components/Nav.svelte
+++ prudential1/src/components/Nav.svelte
@@ -4,11 +4,9 @@
 </script>
 
 <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-light">
-    <div class="navbar-collapse collapse" id="collapsingNavbar">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/" data-toggle="modal"><img src="logo_pru.png" alt="logo_pru" width="150" height="35" ></a>
+                <a class="nav-link" href="/" data-target="#myModal" data-toggle="modal"><img src="logo_pru.png" alt="logo_pru" width="150" height="35" ></a>
             </li>
         </ul>
-    </div>
 </nav>
