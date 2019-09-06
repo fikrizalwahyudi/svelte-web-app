5c24804ecbef6a42947cd00267f5471061582150
luckyramdani007 <luckyramdani007@gmail.com>
2019-09-06 12:53:26 +0700

    fixing navbar image in right position

--- prudential1/src/components/Nav.svelte
+++ prudential1/src/components/Nav.svelte
@@ -3,11 +3,12 @@
 	export let segment;
 </script>
 
-
-<nav class="navbar navbar fixed-top  bg-light">
-	<ul class="nav navbar-nav navbar-right">
-		<li class="pull-sm-right">
-			<img class="float-right" src="logo_pru.png" alt="logo_pru" width="150" height="35" >
-		</li>
-	</ul>
+<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-light">
+    <div class="navbar-collapse collapse" id="collapsingNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/" data-toggle="modal"><img src="logo_pru.png" alt="logo_pru" width="150" height="35" ></a>
+            </li>
+        </ul>
+    </div>
 </nav>
--- prudential1/src/routes/index.svelte
+++ prudential1/src/routes/index.svelte
@@ -3,6 +3,7 @@
 <svelte:head>
 	<title>Verifikasi</title>
 </svelte:head>
+
 <div class="row">
 	<div class="col-12 bg-danger w-100" style="height:100vh;">
 	  <div class="card bg-danger mt-4 pt-5">
@@ -12,5 +13,6 @@
 	  </div>
 	</div>
 </div>
+
 
 	
